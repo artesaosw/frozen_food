05c3de5d91f6d9ce3754ea1f582384368d3f602f
artesaosw <ruben.linssilva@altran.com>
2021-04-29 10:00:55 +0100

    initial commit

---
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/FrozenFoodApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.engineering.ddd.frozen_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrozenFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrozenFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/engineering/ddd/frozen_food/FrozenFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.engineering.ddd.frozen_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrozenFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
