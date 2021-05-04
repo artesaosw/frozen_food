1821930b49c4628a3f3f597bf8f709d7be3e9cbc
MiguelACMF2021 <83582655+MiguelACMF2021@users.noreply.github.com>
2021-05-04 17:20:55 +0100

    testUpdate

---
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/Demanda.java
@@ -0,0 +1,15 @@
+package com.capgemini.engineering.ddd.frozen_food.domain.producao;
+
+import com.capgemini.engineering.ddd.frozen_food.domain.menu.Ingredient;
+//Demanda é apenas de um ingrediente ou conjunto de ingredientes?
+public class Demanda {
+
+    private Unit unit;
+
+    private int quantity;
+
+    private Ingredient;
+
+    public Demanda (){};
+
+}
---
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/ProducedRecipes.java
@@ -0,0 +1,52 @@
+package com.capgemini.engineering.ddd.frozen_food.domain.producao;
+
+import com.capgemini.engineering.ddd.frozen_food.domain._shared.BatchID;
+import com.capgemini.engineering.ddd.frozen_food.domain._shared.Identificator;
+import com.capgemini.engineering.ddd.frozen_food.domain._shared.RecipeID;
+import com.capgemini.engineering.ddd.frozen_food.domain.menu.Recipe;
+import com.capgemini.engineering.ddd.frozen_food.domain.menu.Recipes;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.tomcat.jni.Local;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class ProducedRecipes {
+
+    //ID da Receita Produzida
+    private String recipeID;
+
+    //ID do Lote Produzido
+    private BatchID id;
+
+    //Unidade de Producao
+    private Unit unit;
+
+    //Quantidade Produzida
+    private int quantity;
+
+    //Prazo de Validade
+    private LocalDate prazoValidade;
+
+    //Date de Producao
+    private LocalDate dataProducao;
+
+    //Prazo de validade pode ser determinado na criacao da receita e automatizado na producao atraves da adicao do tempo a data de producao
+    public ProducedRecipes(Unit unit, @Positive int quantity, @NotBlank String recipeID, LocalDate prazoValidade){
+        this.id = Identificator.newInstance(RecipeID.class);
+        this.unit = unit;
+        this.quantity = quantity;
+        this.recipeID = recipeID;
+        this.prazoValidade = prazoValidade;
+        setLocalDate();
+    }
+
+    public void setLocalDate(){
+        this.dataProducao = LocalDate.now();
+    }
+
+}
