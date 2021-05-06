fc02e3151d4964de2cd720f3bc754e1983ee3fec
MiguelACMF2021 <83582655+MiguelACMF2021@users.noreply.github.com>
2021-05-06 15:31:14 +0100

    Interface and new Parameter

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/ProducedRecipes.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/ProducedRecipes.java
@@ -1,58 +1,12 @@
 package com.capgemini.engineering.ddd.frozen_food.domain.producao;
 
+import com.capgemini.engineering.ddd.frozen_food.domain.__metadata.Repository;
 import com.capgemini.engineering.ddd.frozen_food.domain._shared.BatchID;
-import com.capgemini.engineering.ddd.frozen_food.domain._shared.Identificator;
-import com.capgemini.engineering.ddd.frozen_food.domain._shared.RecipeID;
-import com.capgemini.engineering.ddd.frozen_food.domain.menu.Recipe;
-import com.capgemini.engineering.ddd.frozen_food.domain.menu.Recipes;
-import lombok.Getter;
-import lombok.Setter;
-import org.apache.tomcat.jni.Local;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import java.time.LocalDate;
 
-@Getter
-@Setter
-public class ProducedRecipes {
+public interface ProducedRecipes extends Repository<ProducedRecipe, BatchID> {
 
-    //ID da Receita Produzida
-    private String recipeID;
-
-    //ID do Lote Produzido
-    private BatchID id;
-
-    //Unidade de Producao
-    private Unit unit;
-
-    //Quantidade Produzida
-    private int quantity;
-
-    //Prazo de Validade
-    private LocalDate prazoValidade;
-
-    //Date de Producao
-    private LocalDate dataProducao;
-
-    //Prazo de validade pode ser determinado na criacao da receita e automatizado na producao atraves da adicao do tempo a data de producao
-    //atualizacao do lote é manual ou autmatico?
-    public ProducedRecipes(@NotNull Unit unit, @Positive int quantity, @NotBlank String recipeID, LocalDate prazoValidade){
-        this.id = Identificator.newInstance(RecipeID.class);
-        this.unit = unit;
-        this.quantity = quantity;
-        this.recipeID = recipeID;
-        this.prazoValidade = prazoValidade;
-        setLocalDate();
-    }
-
-    public void setLocalDate(){
-        this.dataProducao = LocalDate.now();
-    }
-
-    //Criacao de Lista com ReceitasProduzidas?
-
-
-
+    boolean existsWithName(@NotBlank String name);
 }
+
