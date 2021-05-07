f737455545bf8c12dc0f8154efe0903cdfc2f9c0
iantunes <isabel.antunessilva@altran.com>
2021-05-07 12:56:27 +0100

    Changed RecipeRegistered event to receive a dto instead of a recipe object.

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/menu/domain/RecipeRegistered.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/menu/domain/RecipeRegistered.java
@@ -2,19 +2,18 @@ package com.capgemini.engineering.ddd.frozen_food.domain.menu.domain;
 
 import com.capgemini.engineering.ddd.frozen_food.domain.__metadata.DomainEvent;
 import com.capgemini.engineering.ddd.frozen_food.domain._shared.RecipeID;
+import com.capgemini.engineering.ddd.frozen_food.domain.menu.domain.DTO.Recipe_SalesDTO;
 import com.capgemini.engineering.ddd.frozen_food.domain.menu.domain.entity.Recipe;
 import lombok.Getter;
 
 @Getter
 public class RecipeRegistered extends DomainEvent {
 
-    private RecipeID recipeID;
-    private Recipe recipe;
+    private Recipe_SalesDTO recipe_SalesDTO;
 
-    public RecipeRegistered(RecipeID recipeID, Recipe recipe) {
+    public RecipeRegistered(Recipe_SalesDTO recipe_SalesDTO) {
         super();
-        this.recipeID = recipeID;
-        this.recipe = recipe;
+        this.recipe_SalesDTO = recipe_SalesDTO;
     }
 
 }
