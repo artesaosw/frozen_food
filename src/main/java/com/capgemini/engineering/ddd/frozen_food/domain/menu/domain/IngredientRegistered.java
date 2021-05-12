a37d057d3b727c8bb52535ad8032ee0ef2326ae6
iantunes <isabel.antunessilva@altran.com>
2021-05-12 17:57:33 +0100

    Created Event IngredientsRequested.

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/menu/domain/IngredientRegistered.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/menu/domain/IngredientRegistered.java
@@ -3,7 +3,7 @@ package com.capgemini.engineering.ddd.frozen_food.domain.menu.domain;
 import com.capgemini.engineering.ddd.frozen_food.domain.__metadata.DomainEvent;
 import com.capgemini.engineering.ddd.frozen_food.domain._shared.IngredientID;
 
-public class IngredientRegistered  extends DomainEvent {
+public class IngredientRegistered extends DomainEvent {
 
     private IngredientID ingredientID;
 
