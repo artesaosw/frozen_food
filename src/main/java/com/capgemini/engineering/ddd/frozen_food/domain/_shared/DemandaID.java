9474dfbde7047c66393e7a2ebcf19f9715f898c6
MiguelACMF2021 <83582655+MiguelACMF2021@users.noreply.github.com>
2021-05-13 16:18:54 +0100

    DemandaID Update

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/_shared/DemandaID.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/_shared/DemandaID.java
@@ -1,10 +1,24 @@
 package com.capgemini.engineering.ddd.frozen_food.domain._shared;
 
+import com.sun.istack.NotNull;
+
+import java.io.Serializable;
 import java.util.UUID;
 
-public class DemandaID implements Identificator{
+public class DemandaID implements Identificator, Serializable {
+
+    private UUID id;
+
+    DemandaID(@NotNull UUID id){
+        this.id = id;
+    }
+
+    public DemandaID() {
+        this.id = UUID.randomUUID();
+    }
+
     @Override
     public UUID id() {
-        return null;
+        return id;
     }
 }
--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/repository/Demandas.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/repository/Demandas.java
@@ -2,6 +2,7 @@ package com.capgemini.engineering.ddd.frozen_food.domain.producao.repository;
 
 
 import com.capgemini.engineering.ddd.frozen_food.domain.__metadata.Repository;
+import com.capgemini.engineering.ddd.frozen_food.domain._shared.DemandaID;
 import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.Demanda;
 
 public interface Demandas extends Repository<Demanda, DemandaID> {
