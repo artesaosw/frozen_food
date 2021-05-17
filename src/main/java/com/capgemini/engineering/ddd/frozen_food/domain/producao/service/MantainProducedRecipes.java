18a93892ad6a0ca86fd9d4b3105d915453bd8c1e
MiguelACMF2021 <83582655+MiguelACMF2021@users.noreply.github.com>
2021-05-17 12:05:41 +0100

    Update MantainProducedRecipes.java

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/service/MantainProducedRecipes.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/service/MantainProducedRecipes.java
@@ -3,7 +3,13 @@ package com.capgemini.engineering.ddd.frozen_food.domain.producao.service;
 import com.capgemini.engineering.ddd.frozen_food.domain.Domain;
 import com.capgemini.engineering.ddd.frozen_food.domain.__metadata.DomainServices;
 import com.capgemini.engineering.ddd.frozen_food.domain._shared.BatchID;
+import com.capgemini.engineering.ddd.frozen_food.domain._shared.Unit;
 import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.ProducedRecipes;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
 
 public class MantainProducedRecipes implements DomainServices {
 
@@ -11,7 +17,10 @@ public class MantainProducedRecipes implements DomainServices {
         return Domain.producedRecipes();
     }
 
-    public void registerNew(){};
+    public void registerNew(@NotNull Unit unit, @Positive int quantity, @NotBlank String recipeID, LocalDate prazoValidade, @NotBlank String tipoReceita){
+
+        //
+    }
 
     //Modifica status da Receita Produzida
     public void closeProducedRecipe(BatchID batchID){}
