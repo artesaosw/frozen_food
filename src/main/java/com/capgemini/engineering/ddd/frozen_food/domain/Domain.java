9ae03cf19bc72633871ac2528ee5b0ef0295a198
MiguelACMF2021 <83582655+MiguelACMF2021@users.noreply.github.com>
2021-05-17 12:12:27 +0100

    services update

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/Domain.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/Domain.java
@@ -3,6 +3,7 @@ package com.capgemini.engineering.ddd.frozen_food.domain;
 import com.capgemini.engineering.ddd.frozen_food.domain.menu.Ingredients;
 import com.capgemini.engineering.ddd.frozen_food.domain.menu.Recipes;
 import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.Demanda;
+import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.ProducedRecipe;
 import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.ProducedRecipes;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -20,7 +21,7 @@ public class Domain implements ApplicationContextAware {
         return applicationContext.getBean(Ingredients.class);
     }
 
-    public static ProducedRecipes producedRecipes() { return applicationContext.getBean(ProducedRecipes.class);}
+    public static ProducedRecipe producedRecipe() { return applicationContext.getBean(ProducedRecipes.class);}
 
     public static Demanda demanda() { return applicationContext.getBean(Demanda.class);}
 
--- src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/service/MantainProducedRecipes.java
+++ src/main/java/com/capgemini/engineering/ddd/frozen_food/domain/producao/service/MantainProducedRecipes.java
@@ -1,25 +1,35 @@
 package com.capgemini.engineering.ddd.frozen_food.domain.producao.service;
 
 import com.capgemini.engineering.ddd.frozen_food.domain.Domain;
+import com.capgemini.engineering.ddd.frozen_food.domain.Events;
 import com.capgemini.engineering.ddd.frozen_food.domain.__metadata.DomainServices;
 import com.capgemini.engineering.ddd.frozen_food.domain._shared.BatchID;
 import com.capgemini.engineering.ddd.frozen_food.domain._shared.Unit;
-import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.ProducedRecipes;
+import com.capgemini.engineering.ddd.frozen_food.domain.menu.Recipe;
+import com.capgemini.engineering.ddd.frozen_food.domain.menu.RecipeRegistered;
+import com.capgemini.engineering.ddd.frozen_food.domain.producao.entity.ProducedRecipe;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
-public class MantainProducedRecipes implements DomainServices {
+public class MantainProducedRecipe implements DomainServices {
 
-    private ProducedRecipes producedRecipes(){
-        return Domain.producedRecipes();
+    private ProducedRecipe producedRecipe(){
+        return Domain.producedRecipe();
     }
 
     public void registerNew(@NotNull Unit unit, @Positive int quantity, @NotBlank String recipeID, LocalDate prazoValidade, @NotBlank String tipoReceita){
 
-        //
+        //Instantiate aggregate
+        ProducedRecipe producedRecipe = new ProducedRecipe(unit,quantity,recipeID,prazoValidade,tipoReceita);
+
+        //persists
+        producedRecipe().registerNew(producedRecipe);
+
+        //reports event
+        Events.report(new ProducedRecipeRegistered(producedRecipe.id()));
     }
 
     //Modifica status da Receita Produzida
