193f5cdeb024fdf05e6c9c3124b380338b30edca
JorgePereira09005 <61985975+JorgePereira09005@users.noreply.github.com>
2021-05-28 11:07:28 +0100

    Delete src/main/java/com/capgemini/engineering/ddd/frozen_food/__metadata directory

--- src/main/java/com/capgemini/engineering/ddd/frozen_food/__metadata/DomainServices.java
+++
@@ -1,4 +0,0 @@
-package com.capgemini.engineering.ddd.frozen_food.__metadata;
-
-public interface DomainServices {
-}
--- src/main/java/com/capgemini/engineering/ddd/frozen_food/__metadata/MetadataException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.engineering.ddd.frozen_food.__metadata;
-
-public class MetadataException extends RuntimeException{
-    public MetadataException(Throwable cause) {
-        super(cause);
-    }
-}
